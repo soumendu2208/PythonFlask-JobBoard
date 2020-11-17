f92f8f0dfb71f18e843e6ac49ed3d8f38d1bdb7e
soumendu2208 <soumendudas.work@gmail.com>
2020-11-18 03:39:44 +0530

    [🎉] Integrated sqlite in UILite

--- jobs/app.py
+++ jobs/app.py
@@ -1,11 +1,37 @@
 # Import modules
-from flask import render_template, Flask
+from flask import render_template, Flask, g
+import sqlite3
 
+PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
+
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit is True:
+        return connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    return results
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 
 @app.route('/')
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
-
