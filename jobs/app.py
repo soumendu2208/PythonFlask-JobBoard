cda81a52adbe3ced4bd35488bfa1eeb751270187
soumendu2208 <soumendudas.work@gmail.com>
2020-11-18 21:37:54 +0530

    [🎉] Modified employer.html, review.html added for the reviews tab.

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,7 @@
 # Import modules
-from flask import render_template, Flask, g
+from flask import render_template, Flask, g, request, redirect, url_for
 import sqlite3
+import datetime
 
 PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
@@ -59,3 +60,20 @@ def employer(employer_id):
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = '
                           'review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
+def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
+                    (review, rating, title, date, status, employer_id), commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template('review.html', employer_id=employer_id)
